e938904cb73e9600f763880978cdd1ec636b74df
Rutuja <jrutuja.n@gmail.com>
2023-11-20 23:13:20 +0530

    nothing

--- Routes/User.js
+++ Routes/User.js
@@ -26,8 +26,8 @@ const storage = multer.diskStorage({
   
 
 router.post('/Register', async (req,res)=>{
-    const {name,password,email,pushToken}=req.body;
-    const user = new GreenUser({username:name,password:password,email:email,pushToken:pushToken})
+    const {name,password,email,pushToken,latitude,longitude}=req.body;
+    const user = new GreenUser({username:name,password:password,email:email,pushToken:pushToken,'homelocation.latitude':latitude,'homelocation.longitude':longitude})
     await user.save().then((doc)=>{
         console.log('User Saved');
         res.json({doc:doc})
